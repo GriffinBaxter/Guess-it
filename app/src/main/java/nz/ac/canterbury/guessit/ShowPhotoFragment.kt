0375e5a0e4772672c58b8df91613321554e60c6c
Griffin Baxter <grb96@uclive.ac.nz>
2022-09-23 15:52:00 +1200

    Added photo choosing (no functionality, only brings up device's photos to pick from)

--- app/src/main/java/nz/ac/canterbury/guessit/ShowPhotoFragment.kt
+++ app/src/main/java/nz/ac/canterbury/guessit/ShowPhotoFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Button
 
 private const val REQUEST_CAMERA = 110
+private const val REQUEST_GALLERY = 111
 
 class ShowPhotoFragment : Fragment() {
 
@@ -24,11 +25,32 @@ class ShowPhotoFragment : Fragment() {
             takePhoto()
         }
 
+        val choosePhotoButton: Button = view.findViewById(R.id.choosePhotoButton)
+        choosePhotoButton.setOnClickListener {
+            choosePhotoFromPhone()
+        }
+
+        val galleryButton: Button = view.findViewById(R.id.galleryButton)
+        galleryButton.setOnClickListener {
+            gallery()
+        }
+
         return view
     }
 
     private fun takePhoto() {  // TODO: add photo to app gallery
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         startActivityForResult(intent, REQUEST_CAMERA)
+    }
+
+    private fun choosePhotoFromPhone() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_GALLERY)
+    }
+
+    private fun gallery() {
+        // TODO
     }
 }
