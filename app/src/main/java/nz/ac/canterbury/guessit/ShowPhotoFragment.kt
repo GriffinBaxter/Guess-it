c55c33049c181f4edcea6b81c30b29b2f632c04d
Griffin Baxter <grb96@uclive.ac.nz>
2022-09-23 15:44:26 +1200

    Added photo taking (no functionality, only brings up device camera app)

--- app/src/main/java/nz/ac/canterbury/guessit/ShowPhotoFragment.kt
+++ app/src/main/java/nz/ac/canterbury/guessit/ShowPhotoFragment.kt
@@ -1,10 +1,15 @@
 package nz.ac.canterbury.guessit
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+
+private const val REQUEST_CAMERA = 110
 
 class ShowPhotoFragment : Fragment() {
 
@@ -12,6 +17,18 @@ class ShowPhotoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_show_photo, container, false)
+        val view = inflater.inflate(R.layout.fragment_show_photo, container, false)
+
+        val takePhotoButton: Button = view.findViewById(R.id.takePhotoButton)
+        takePhotoButton.setOnClickListener {
+            takePhoto()
+        }
+
+        return view
+    }
+
+    private fun takePhoto() {  // TODO: add photo to app gallery
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, REQUEST_CAMERA)
     }
 }
