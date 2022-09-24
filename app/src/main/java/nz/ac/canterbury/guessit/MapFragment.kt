0d2a7bd04190a1fef5ab53c22bafdeaf66c8a0e2
Rugter van Kruiningen <rva42@uclive.ac.nz>
2022-09-24 12:11:52 +1200

    Added guess button to map and added logic to make sure a point is selected. Also changed the map to metric from imperial units

--- app/src/main/java/nz/ac/canterbury/guessit/MapFragment.kt
+++ app/src/main/java/nz/ac/canterbury/guessit/MapFragment.kt
@@ -1,28 +1,31 @@
 package nz.ac.canterbury.guessit
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.mapbox.geojson.Point
 import com.mapbox.maps.MapView
 import com.mapbox.maps.MapboxMap
-import com.mapbox.maps.ScreenCoordinate
 import com.mapbox.maps.Style
 import com.mapbox.maps.plugin.annotation.annotations
 import com.mapbox.maps.plugin.annotation.generated.CircleAnnotationOptions
 import com.mapbox.maps.plugin.annotation.generated.createCircleAnnotationManager
 import com.mapbox.maps.plugin.gestures.addOnMapClickListener
+import com.mapbox.maps.plugin.scalebar.scalebar
 
 
 class MapFragment : Fragment() {
 
 
-    lateinit var latLongLabel: TextView
+    lateinit var map_guessButton: Button
     lateinit var mapView: MapView
     lateinit var mapboxMap: MapboxMap
+    lateinit var selectedPoint: Point
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,27 +36,26 @@ class MapFragment : Fragment() {
         val view: View = inflater.inflate(R.layout.fragment_map, container, false)
 
 
-        latLongLabel = view.findViewById(R.id.latLongLabel)
 
 
         mapView = view.findViewById(R.id.mapView)
         mapboxMap = mapView.getMapboxMap()
         mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
+        mapView.scalebar.isMetricUnits = true
 
         // Create an instance of the Annotation API and get the CircleAnnotationManager.
         val annotationApi = mapView.annotations
         val circleAnnotationManager = annotationApi.createCircleAnnotationManager()
 
         mapboxMap.addOnMapClickListener { point ->
-
-            Toast.makeText(activity as MainActivity, String.format("User clicked at: %s", point.toString()), Toast.LENGTH_LONG).show()
+            selectedPoint = point
 
             //Adding marker to map
             circleAnnotationManager.deleteAll()
             // Set options for the resulting circle layer.
             val circleAnnotationOptions: CircleAnnotationOptions = CircleAnnotationOptions()
                 // Define a geographic coordinate.
-                .withPoint(point)
+                .withPoint(selectedPoint)
                 // Style the circle that will be added to the map.
                 .withCircleRadius(8.0)
                 .withCircleColor("#3399ff")
@@ -62,15 +64,26 @@ class MapFragment : Fragment() {
             // Add the resulting circle to the map.
             circleAnnotationManager.create(circleAnnotationOptions)
 
-
-            latLongLabel.text = "Latitude: ${point.latitude()}\nLongitude: ${point.longitude()}"
             true
+        }
+
+        map_guessButton = view.findViewById(R.id.map_guessButton)
+        map_guessButton.setOnClickListener {
+            if (!this::selectedPoint.isInitialized) {
+                Toast.makeText(activity as MainActivity, "Please select a point on the map to guess", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                manageGuess()
+            }
         }
 
 
         return view
     }
 
+    private fun manageGuess() {
+        Toast.makeText(activity as MainActivity, "Latitude: ${selectedPoint.latitude()}\nLongitude: ${selectedPoint.longitude()}", Toast.LENGTH_SHORT).show()
+    }
 
 
 
