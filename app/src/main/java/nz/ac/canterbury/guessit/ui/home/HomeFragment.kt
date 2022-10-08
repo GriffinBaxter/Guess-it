2180deb8c7c3e345553ee282766db65b530cb80f
Griffin Baxter <grb96@uclive.ac.nz>
2022-10-08 21:12:23 +1300

    Implemented action bar with preferences button and navigation.

--- app/src/main/java/nz/ac/canterbury/guessit/ui/home/HomeFragment.kt
+++ app/src/main/java/nz/ac/canterbury/guessit/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import com.google.android.material.appbar.MaterialToolbar
 import nz.ac.canterbury.guessit.R
 
 class HomeFragment : Fragment() {
@@ -35,6 +36,19 @@ class HomeFragment : Fragment() {
         val showNearbyButton: Button = view.findViewById(R.id.showNearbyButton)
         showNearbyButton.setOnClickListener {
             Navigation.findNavController(requireView()).navigate(R.id.action_homeFragment_to_nearbyFragment)
+        }
+
+        val actionBar: MaterialToolbar = view.findViewById(R.id.actionBar)
+        actionBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.preferencesButton -> {
+                    Navigation.findNavController(requireView()).navigate(R.id.action_homeFragment_to_preferencesFragment)
+                    true
+                }
+                else -> {
+                    super.onOptionsItemSelected(menuItem)
+                }
+            }
         }
 
         return view
