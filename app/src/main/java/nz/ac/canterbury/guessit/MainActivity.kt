ba6e289db82a07e35f45a7fa544a9eea3654a5d8
Griffin Baxter <grb96@uclive.ac.nz>
2022-10-09 20:59:16 +1300

    Implemented dark mode preference functionality.

--- app/src/main/java/nz/ac/canterbury/guessit/MainActivity.kt
+++ app/src/main/java/nz/ac/canterbury/guessit/MainActivity.kt
@@ -1,7 +1,10 @@
 package nz.ac.canterbury.guessit
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,6 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val darkMode = preferences.getBoolean("darkMode", false)
+        if (darkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
--- app/src/main/java/nz/ac/canterbury/guessit/controller/SettingsFragment.kt
+++ app/src/main/java/nz/ac/canterbury/guessit/controller/SettingsFragment.kt
@@ -1,11 +1,24 @@
 package nz.ac.canterbury.guessit.controller
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.CheckBoxPreference
 import androidx.preference.PreferenceFragmentCompat
 import nz.ac.canterbury.guessit.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        findPreference<CheckBoxPreference>("darkMode")?.let {
+            it.setOnPreferenceChangeListener { _, newValue ->
+                if (newValue == true) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+                true
+            }
+        }
     }
 }
